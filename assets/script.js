d163923e8bf7416c3d82544bcb8e940dc123d542
Tay Starr <tstarrroberts@gmail.com>
2022-03-04 19:25:32 -0500

    question skeleton add

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
+function generatePassword() {
 
+  window.alert("Welcome to the password generator! Just a few questions to make your perfect password!")
+
+  var passwordLength = window.prompt("How long do you want your password to be? Choose a number between 8 and 128.");
+
+  var numCharacter = window.confirm("Do you want numbers in your password?");
+
+  var specCharacter = window.confirm("Do you want special characters in your password?");
+
+  var lowCharacter = window.confirm("Do you want lowercase characters in your password?");
+
+  var upCharacter = window.confirm("Do you want uppercase characters in your password?");
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
