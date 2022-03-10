d863e1a273183962a5079349103d7b788718e5c3
Tay Starr <tstarrroberts@gmail.com>
2022-03-10 16:56:09 -0500

    generator added arrays made to strings

--- assets/script.js
+++ assets/script.js
@@ -3,46 +3,51 @@ function generatePassword() {
 
   window.alert("Welcome to the password generator! Just a few questions to make your perfect password!")
 
-  getPasswordLength();
-
   passwordQuestions();
 
-}
+  // return generated password
+  return password;
+
+};
+
 
 function passwordQuestions() {
 
+  var length = " ";
+
+  while (length === "" || length === null || length > 128 || length < 8) {
+    length = prompt("How long do you want your password to be? Choose a number between 8 and 128.");
+  };
+  var usableCharacters = "";
+
   var numConfirm = window.confirm("Do you want numbers in your password?");
-  var arrayNum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-  console.log(arrayNum);
   if (numConfirm) {
-    window.alert("Numbers will be added to your password.");
+    window.alert("Numbers will be added to your password.")
+    usableCharacters += "1234567890";
   } else {
     window.alert("No numbers will be added to you password.")
   }
 
   var specCharacter = window.confirm("Do you want special characters in your password?");
-  var arraySpecChar = ['!', '"', '#', '$', '%', '&', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~'];
-  console.log(arraySpecChar);
     if (specCharacter) {
-      window.alert("Special characters will be added to your password.");
+      window.alert("Special characters will be added to your password.")
+      usableCharacters += "`~!@#$%^&*()_-+={}[]:;'?/><|";
     } else {
       window.alert("No special characters will be added to your password.");
     }
 
   var lowCharacter = window.confirm("Do you want lowercase characters in your password?");
-  var arrayLow = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  console.log(arrayLow);
     if (lowCharacter) {
-      window.alert("Lower case characters will be added to your password.");
+      window.alert("Lower case characters will be added to your password.")
+      usableCharacters += "abcdefghijklmnopqrstuvwxyz";
     } else {
       window.alert("Lower case characters will not be added to your password.");
     }
 
   var upCharacter = window.confirm("Do you want uppercase characters in your password?");
-  var arrayUp = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  console.log(arrayUp);
     if (upCharacter) {
-      window.alert("Upper case characters will be added to your password.");
+      window.alert("Upper case characters will be added to your password.")
+      usableCharacters += "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     } else {
       window.alert("Upper case characters will not be added to your password.");
     }
@@ -52,23 +57,14 @@ function passwordQuestions() {
     window.alert("You must choose at least one option! Please try again.");
     return passwordQuestions();
     }
+console.log(usableCharacters);
+
+  password = "";
+  for (var i = 0; i < length; i++) {
+    password += usableCharacters[Math.floor(Math.random() * length)];
 }
-
-var getPasswordLength = function() {
-  var length = "";
-
-  while (length === "" || length === null || length > 128 || length < 8) {
-    length = prompt("How long do you want your password to be? Choose a number between 8 and 128.");
-  }
-  console.log("Password length is " + length);
-  return length;
-  // else {
-  //   window.alert("You need to provide a valid number! Please try again.");
-  //   return getPasswordLength();
-  // }
-
-}
-
+  return password;
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
